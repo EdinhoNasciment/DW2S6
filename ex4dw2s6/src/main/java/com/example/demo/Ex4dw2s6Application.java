6dedecb5c6d37363d9be05dd9881c0e01e69ed42
EdinhoNasciment <112394149+EdinhoNasciment@users.noreply.github.com>
2024-11-01 07:20:45 -0700

    Add files via upload

---
+++ ex4dw2s6/src/main/java/com/example/demo/Ex4dw2s6Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4dw2s6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex4dw2s6Application.class, args);
+	}
+
+}
---
+++ ex4dw2s6/src/main/java/com/example/demo/resource/UserResource.java
@@ -0,0 +1,50 @@
+package com.example.demo.resource;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.model.User;
+import com.example.demo.repository.UserRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/all")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	@PostMapping("/create")
+	@ResponseStatus(HttpStatus.CREATED)
+	public User create(@Valid @RequestBody User user, HttpServletResponse response) {
+		return userRepository.save(user);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		Optional<User> user = userRepository.findById(id);
+		if(user.isPresent()) {
+			return ResponseEntity.ok(user.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+}
